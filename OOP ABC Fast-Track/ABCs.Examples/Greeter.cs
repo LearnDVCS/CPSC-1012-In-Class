5b9a7c70ff3b828c3ba1727984b9cd2eb7c81390
Dan Gilleland <dgilleland@nait.ca>
2016-09-14 15:10:02 -0600

    Fast Track example

---
+++ OOP ABC Fast-Track/ABCs.Examples/Greeter.cs
@@ -0,0 +1,29 @@
+﻿namespace ABCs.Examples
+{
+    public class Greeter
+    {
+        // Fields (to hold information)
+        private string _WelcomeMessage;
+        private string _FarewellMessage;
+
+        // Constructor (to set up the data for objects)
+        public Greeter(string welcomeMsg, string farewellMsg)
+        {
+            // The job of a constructor is to make sure that
+            // all the fields/properties have "meaningful" values
+            this._WelcomeMessage = welcomeMsg;
+            this._FarewellMessage = farewellMsg;
+        }
+
+        // Methods
+        public string SayHello()
+        {
+            return _WelcomeMessage;
+        }
+
+        public string SayGoodbye()
+        {
+            return _FarewellMessage;
+        }
+    }
+}
---
+++ OOP ABC Fast-Track/ABCs.Examples/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABCs.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring a variable will give me a "place" to hold information
+            int myAge = 32; // Variable Initialization statement
+            int averageClassAge; // Variable Declaration statement
+
+            // Assignment statement
+            averageClassAge = 23;
+
+            // Output what the maximum integer value is that I can store in
+            // an int variable
+            Console.WriteLine("Max int value is: " + int.MaxValue);
+            Console.WriteLine("Min int value is: " + int.MinValue);
+
+            long totalDistinctValues = (long) int.MaxValue - int.MinValue;
+            Console.WriteLine("There are " + totalDistinctValues + " total distinct values that an integer can hold.");
+
+
+            // Create two objects based on the Greeter data type (class)
+            // Declaring a variable to hold a Greeter object
+            Greeter walmartGreeter;
+            // Create the Greeter object and assign it to the variable
+            walmartGreeter = new Greeter("Welcome to Walmart!", "Thank you for shopping at Walmart! Come again!!");
+            Console.WriteLine(walmartGreeter.SayHello());
+            Console.WriteLine(walmartGreeter.SayGoodbye());
+
+            Greeter klingonCommander;
+            klingonCommander = new Greeter("nuqneH!", "Qapla'!");
+            Console.WriteLine(klingonCommander.SayHello());
+            Console.WriteLine(klingonCommander.SayGoodbye());
+
+        }
+    }
+}
