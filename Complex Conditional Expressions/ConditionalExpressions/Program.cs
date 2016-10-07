d9ee5a0ac144ac14ae42f70b45f8ccbeb8931b54
Dan Gilleland <dgilleland@nait.ca>
2016-10-07 13:43:19 -0600

    Complex Conditional Expressions

---
+++ Complex Conditional Expressions/ConditionalExpressions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConditionalExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an angle (in degrees): ");
+            int angle;
+            angle = int.Parse(Console.ReadLine());
+            string angleType;
+            if (angle > 0 && angle < 90)
+                angleType = "Acute";
+            else if (angle == 90)
+                angleType = "Right";
+            else if (angle > 90 && angle < 180)
+                angleType = "Obtuse";
+            else if (angle == 180)
+                angleType = "Straight";
+            else if (angle > 180 && angle < 360)
+                angleType = "Reflex";
+            else if (angle == 360)
+                angleType = "Full Rotation";
+            else
+                angleType = "Undefined";
+
+            Console.WriteLine($"A {angle} degree angle is a {angleType} angle.");
+
+            if(angleType != "Undefined")
+            {
+                double radians = angle * (Math.PI / 180);
+                radians = Math.Round(radians, 3);
+                Console.WriteLine($"A {angle} degree angle is {radians} Radians.");
+            }
+        }
+    }
+}
