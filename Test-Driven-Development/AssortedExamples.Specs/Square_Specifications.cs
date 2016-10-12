674c4dfc04edf174a5d50151f3ef5cd06c24d544
Dan Gilleland <dgilleland@nait.ca>
2016-10-12 16:42:59 -0600

    Unit Test Sample

---
+++ Test-Driven-Development/AssortedExamples.Specs/Square_Specifications.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssortedExamples; // get the Square class from the other project
+using Xunit;
+
+namespace AssortedExamples.Specs
+{
+    public class Square_Specifications
+    {
+        [Fact]
+        public void Should_Store_Side_Length()
+        {
+            // Arrange
+            Square objectBeingTested;
+            int expectedSideLength = 5;
+            // Act
+            objectBeingTested = new Square(expectedSideLength);
+            // Assert
+            Assert.Equal(expectedSideLength, objectBeingTested.Side);
+        }
+
+        //[Fact] // An annotation for something always true
+        [Theory]
+        [InlineData(5, 25)]
+        [InlineData(1, 1)]
+        [InlineData(3, 9)]
+        public void Should_Calculate_Area(int sideLength,
+            int expectedArea)
+        {
+            // Arrange
+            Square sut; // Situation Under Test
+            
+            // Act
+            sut = new Square(sideLength);
+            // Assert
+            Assert.Equal(expectedArea, sut.Area);
+        }
+
+        [Theory]
+        [InlineData(1, 4)]
+        [InlineData(2, 8)]
+        [InlineData(3, 12)]
+        public void Should_Calculate_Perimeter(int givenSideLength, int expectedPerimeter)
+        {
+            // Arrange
+            Square sut;
+            // Act
+            sut = new Square(givenSideLength);
+            // Assert
+            Assert.Equal(expectedPerimeter, sut.Perimeter);
+        }
+    }
+}
---
+++ Test-Driven-Development/AssortedExamples/Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AssortedExamples
+{
+    public class Square
+    {
+        // Properties
+        public int Side { get; private set; }
+        public int Area
+        {
+            get
+            {
+                return Side * Side;
+            }
+        }
+        public Square(int sideLength)
+        {
+            Side = sideLength;
+        }
+    }
+}
