871fd7f695b9646ca68752b28bbd0db9700233b1
Dan Gilleland <dgilleland@nait.ca>
2016-10-17 11:58:26 -0600

    Enumerations and Composition

---
+++ Adding Complexity/EnumAndComposition/Address.cs
@@ -0,0 +1,21 @@
+﻿namespace EnumAndComposition
+{
+    public class Address
+    {
+        public string Street { get; private set; }
+        public string Unit { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string ZipCode { get; private set; }
+
+        public Address(string unit, string street, string city, string state,
+                string zipCode)
+        {
+            this.Unit = unit;
+            this.Street = street;
+            this.City = city;
+            this.State = state;
+            this.ZipCode = zipCode;
+        }
+    }
+}
---
+++ Adding Complexity/EnumAndComposition/Coin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EnumAndComposition
+{
+    public class Coin
+    {
+        // Declare my CoinFace inside of Coin
+        public enum CoinFace { HEADS, TAILS }
+
+        public CoinFace FaceShowing { get; private set; }
+
+        public Coin()
+        {
+            Toss();
+        }
+
+        private static Random _rnd = new Random();
+
+        public void Toss()
+        {
+            if (_rnd.Next(2) == 0)
+                FaceShowing = CoinFace.HEADS;
+            else
+                FaceShowing = CoinFace.TAILS;
+        }
+    }
+}
