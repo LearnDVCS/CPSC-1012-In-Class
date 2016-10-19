b8922d9691ab593110ff81f6c0bfb82f0e414a42
Dan Gilleland <dgilleland@nait.ca>
2016-10-19 15:04:10 -0600

    Composition

--- Adding Complexity/EnumAndComposition/Program.cs
+++ Adding Complexity/EnumAndComposition/Program.cs
@@ -10,6 +10,36 @@ namespace EnumAndComposition
     {
         static void Main(string[] args)
         {
+            DemoStudentComposition();
+        }
+
+        private static void DemoStudentComposition()
+        {
+            // Addresses courtesy of http://www.fakeaddressgenerator.com/
+            Address myAddress = new Address(null,
+                                            "4964 Roger Street",
+                                            "Duncan",
+                                            "British Columbia",
+                                            "V9L 1E5");
+            Student me, roomate;
+            me = new Student(123456789,
+                             "Dan (the man) Gilleland",
+                             Gender.Male,
+                             myAddress,
+                             "DMIT",
+                             9.0,
+                             false);
+
+            roomate = new Student(987654321,
+                                  "Phil Errup",
+                                  Gender.Male,
+                                  myAddress,
+                                  "DMIT",
+                                  7.8);
+
+            // Display the information about a student
+            Console.WriteLine(me.ToString());
+            Console.WriteLine(roomate.ToString());
         }
     }
 }
---
+++ Adding Complexity/EnumAndComposition/Student.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace EnumAndComposition
+{
+    public class Student
+    {
+        #region Fields
+        private string _name; // The full name of the student
+        private string _program; // The course program; e.g.: "DMIT"
+        private double _gradePointAverage; // GPA is from 1.0 to 9.0
+        private Address _homeAddress;
+        #endregion
+
+        #region Properties
+        public int StudentId { get; private set; }
+        public bool IsFullTime { get; set; }
+        public Gender Gender { get; set; }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception("Name cannot be empty");
+                _name = value.Trim();
+            }
+        }
+
+        public string Program
+        {
+            get { return _program; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) ||
+                    string.IsNullOrEmpty(value.Trim()))
+                    throw new Exception("Program cannot be empty");
+                _program = value.Trim();
+            }
+        }
+
+        public Address HomeAddress
+        {
+            get { return _homeAddress; }
+            set
+            {
+                if (value == null)
+                    throw new Exception("Address is required");
+                _homeAddress = value;
+            }
+        }
+
+        public double GradePointAverage
+        {
+            get { return _gradePointAverage; }
+            set
+            {
+                if (value < 1.0 || value > 9.0)
+                    throw new Exception("GPA must be between 1 and 9 inclusive");
+                _gradePointAverage = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        // Greedy Constructor
+        public Student(int id, 
+                       string fullName,
+                       Gender gender,
+                       Address homeAddress,
+                       string programOfStudy,
+                       double gradePointAverage,
+                       bool isFullTime = true)
+        {
+            // My job is to ensure that "valid values" are in all
+            // the properties/fields
+            if (id < 100000000 || id > 999999999)
+                throw new Exception("Student Ids must be 9 digits");
+            StudentId = id;
+            Name = fullName;
+            Gender = gender;
+            HomeAddress = homeAddress;
+            Program = programOfStudy;
+            GradePointAverage = gradePointAverage;
+            IsFullTime = isFullTime;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"({StudentId}) {Name}";
+        }
+        #endregion
+    }
+}
