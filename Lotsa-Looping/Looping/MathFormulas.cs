5276d4c3597af92b4cf5cf06bc1c854e9b4cfc6c
Dan Gilleland <dgilleland@nait.ca>
2016-10-19 15:05:19 -0600

    Looping Samples

---
+++ Lotsa-Looping/Looping/MathFormulas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Looping
+{
+    public class MathFormulas
+    {
+        /// <summary>
+        /// Calculates the number at a certain position in the
+        /// Fibonacci Sequence.
+        /// </summary>
+        /// <param name="i">
+        /// The position in the sequence (Must be greater than zero)
+        /// </param>
+        /// <returns>A value in the  Fibonacci Sequence</returns>
+        public static int FibonacciNumber(int i)
+        {
+            int current = 1, previous = 1, beforePrevious = 0;
+            if (i >= 1)
+            {
+                for (int counter = 3; counter <= i; counter++)
+                {
+                    beforePrevious = previous;
+                    previous = current;
+                    current = beforePrevious + previous;
+                }
+            }
+            else
+                throw new Exception("Can only create a fibonacci  number based on a positive non-zero position");
+            return current;
+        }
+    }
+}
