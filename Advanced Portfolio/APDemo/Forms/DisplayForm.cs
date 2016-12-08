e734f7231f6fe60be0f0ccd13b132fc19194c7b5
Dan Gilleland <dgilleland@nait.ca>
2016-12-08 11:19:55 -0700

    Lab and Win Forms Demo

---
+++ Advanced Portfolio/APDemo/Forms/DisplayForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APDemo.Forms
+{
+    public class DisplayForm
+    {
+        public void ShowForm(List<MultipleChoiceQuestion> questions)
+        {
+            // TODO: Loop through the questions and display them to the user
+        }
+    }
+}
---
+++ Advanced Portfolio/APDemo/MultipleChoiceQuestion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APDemo
+{
+    public class MultipleChoiceQuestion
+    {
+        // TODO: Your student work goes here.....
+    }
+}
